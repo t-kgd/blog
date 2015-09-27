87d3162b78f7f70b555b4b31249400cecf5ae94d
misakura <t-kgd@users.noreply.github.com>
2015-09-28 04:04:58 +0900

    clean オーバーロードメソッドを整理。

--- dynamicproxy/dynamicproxy/src/main/java/jp/gr/java_conf/kgd/example/blog/dynamicproxy/ProxyUtil.java
+++ dynamicproxy/dynamicproxy/src/main/java/jp/gr/java_conf/kgd/example/blog/dynamicproxy/ProxyUtil.java
@@ -34,10 +34,24 @@ import java.util.function.Supplier;
 
 public class ProxyUtil {
 
-    // [private]
-    private static <T> ProxyHolder<T> createProxyHolder(Supplier<? extends T> lazyWrapped, Class<?> clazz) {
+    // [private] リスナを外から渡すパターン
+    private static <T> T createProxy(Supplier<? extends T> lazyWrapped, Class<?> clazz,
+                                     List<OnPreInvokeListener<? super T>> onPreInvokeListeners,
+                                     List<OnPostInvokeListener<? super T>> onPostInvokeListeners,
+                                     List<ErrorHandler<? super T>> errorHandlers) {
         Class<?>[] interfaces = ReflectionUtil.extractInterfaces(clazz);
-        Handler<T> handler = new Handler<>(lazyWrapped);
+        Handler<T> handler = new Handler<>(lazyWrapped, onPreInvokeListeners, onPostInvokeListeners, errorHandlers);
+        T proxy = (T) Proxy.newProxyInstance(clazz.getClassLoader(), interfaces, handler);
+        return proxy;
+    }
+
+    // [private] リスナを後から取り出せるようにするパターン
+    private static <T> ProxyHolder<T> createProxyHolder(Supplier<? extends T> lazyWrapped, Class<?> clazz,
+                                                        List<OnPreInvokeListener<? super T>> onPreInvokeListeners,
+                                                        List<OnPostInvokeListener<? super T>> onPostInvokeListeners,
+                                                        List<ErrorHandler<? super T>> errorHandlers) {
+        Class<?>[] interfaces = ReflectionUtil.extractInterfaces(clazz);
+        Handler<T> handler = new Handler<>(lazyWrapped, onPreInvokeListeners, onPostInvokeListeners, errorHandlers);
         T proxy = (T) Proxy.newProxyInstance(clazz.getClassLoader(), interfaces, handler);
         ProxyHolder<T> proxyHolder = new SimpleProxyHolder<>(proxy, handler);
         return proxyHolder;
@@ -48,6 +62,66 @@ public class ProxyUtil {
      * <p>
      * 任意のインスタンスをラップし、メソッドの前後に処理を挟むことのできるプロキシを生成します。
      * <code>lazyWrapped#get</>は、プロキシのメソッドが呼びだされるたびに呼び出されます。
+     * 各リスナーのListインスタンスはそのままプロキシに参照されるので、要素に変更を加えればプロキシのリスナを変更することができます。
+     *
+     * @param lazyWrapped
+     * @param onPreInvokeListeners
+     * @param onPostInvokeListeners
+     * @param errorHandlers
+     * @param dummy                 型情報を受け取るためのダミーの引数。
+     * @param <T>                   インターフェースである必要があります。
+     * @return 生成したプロキシ。
+     */
+    public static <T> T createLazyProxy(Supplier<? extends T> lazyWrapped,
+                                        List<OnPreInvokeListener<? super T>> onPreInvokeListeners,
+                                        List<OnPostInvokeListener<? super T>> onPostInvokeListeners,
+                                        List<ErrorHandler<? super T>> errorHandlers,
+                                        T... dummy) {
+        return createProxy(lazyWrapped, dummy.getClass().getComponentType(), onPreInvokeListeners, onPostInvokeListeners, errorHandlers);
+    }
+
+    /**
+     * 動的プロキシを生成する。
+     * <p>
+     * 任意のインスタンスをラップし、メソッドの前後に処理を挟むことのできるプロキシを生成します。
+     * <code>lazyWrapped#get</>は、プロキシのメソッドが呼びだされるたびに呼び出されます。
+     * 生成したプロキシは{@link ProxyHolder}に包含されて返されます。
+     *
+     * @param lazyWrapped
+     * @param onPreInvokeListeners
+     * @param onPostInvokeListeners
+     * @param errorHandlers
+     * @param dummy                 型情報を受け取るためのダミーの引数。
+     * @param <T>                   インターフェースである必要があります。
+     * @return 生成したプロキシを包含したインスタンス。
+     */
+    public static <T> ProxyHolder<T> createLazyProxyHolder(Supplier<? extends T> lazyWrapped,
+                                                           List<OnPreInvokeListener<? super T>> onPreInvokeListeners,
+                                                           List<OnPostInvokeListener<? super T>> onPostInvokeListeners,
+                                                           List<ErrorHandler<? super T>> errorHandlers,
+                                                           T... dummy) {
+        return createProxyHolder(lazyWrapped, dummy.getClass().getComponentType(), onPreInvokeListeners, onPostInvokeListeners, errorHandlers);
+    }
+
+
+    /**
+     * 動的プロキシを生成する。
+     * <p>
+     * 空のリスナーListを生成して{@link #createLazyProxy(Supplier, List, List, List, Object[])}に委譲します。
+     *
+     * @param lazyWrapped
+     * @param dummy       型情報を受け取るためのダミーの引数。
+     * @param <T>         インターフェースである必要があります。
+     * @return 生成したプロキシを包含したインスタンス。
+     */
+    public static <T> T createLazyProxy(Supplier<? extends T> lazyWrapped, T... dummy) {
+        return createProxy(lazyWrapped, dummy.getClass().getComponentType(), new LinkedList<>(), new LinkedList<>(), new LinkedList<>());
+    }
+
+    /**
+     * 動的プロキシを生成する。
+     * <p>
+     * 空のリスナーListを生成して{@link #createLazyProxyHolder(Supplier, List, List, List, Object[])}に委譲します。
      *
      * @param lazyWrapped
      * @param dummy       型情報を受け取るためのダミーの引数。
@@ -55,39 +129,29 @@ public class ProxyUtil {
      * @return 生成したプロキシを包含したインスタンス。
      */
     public static <T> ProxyHolder<T> createLazyProxyHolder(Supplier<? extends T> lazyWrapped, T... dummy) {
-        return createProxyHolder(lazyWrapped, dummy.getClass().getComponentType());
+        return createProxyHolder(lazyWrapped, dummy.getClass().getComponentType(), new LinkedList<>(), new LinkedList<>(), new LinkedList<>());
     }
 
-    /**
-     * 動的プロキシを生成する。
-     * <p>
-     * 任意のインスタンスをラップし、メソッドの前後に処理を挟むことのできるプロキシを生成します。
-     *
-     * @param wrapped
-     * @param <T>
-     * @return
-     */
+    public static <T> T createProxy(T wrapped,
+                                    List<OnPreInvokeListener<? super T>> onPreInvokeListeners,
+                                    List<OnPostInvokeListener<? super T>> onPostInvokeListeners,
+                                    List<ErrorHandler<? super T>> errorHandlers) {
+        return createProxy(() -> wrapped, wrapped.getClass(), onPreInvokeListeners, onPostInvokeListeners, errorHandlers);
+    }
+
+    public static <T> ProxyHolder<T> createProxyHolder(T wrapped,
+                                                       List<OnPreInvokeListener<? super T>> onPreInvokeListeners,
+                                                       List<OnPostInvokeListener<? super T>> onPostInvokeListeners,
+                                                       List<ErrorHandler<? super T>> errorHandlers) {
+        return createProxyHolder(() -> wrapped, wrapped.getClass(), onPreInvokeListeners, onPostInvokeListeners, errorHandlers);
+    }
+
+    public static <T> T createProxy(T wrapped) {
+        return createProxy(() -> wrapped, wrapped.getClass(), new LinkedList<>(), new LinkedList<>(), new LinkedList<>());
+    }
+
     public static <T> ProxyHolder<T> createProxyHolder(T wrapped) {
-        return createProxyHolder(() -> wrapped, wrapped.getClass());
-    }
-
-    /**
-     * 動的プロキシを生成する。
-     * <p>
-     * {@link #createProxyHolder(Object)}の簡易版です。
-     * あらかじめ、メソッドの前後に挟みこむ処理を指定して、プロキシを生成します。
-     *
-     * @param wrapped
-     * @param onPreInvokeListener
-     * @param onPostInvokeListener
-     * @param <T>
-     * @return
-     */
-    public static <T> T createProxy(T wrapped, OnPreInvokeListener<? super T> onPreInvokeListener, OnPostInvokeListener<? super T> onPostInvokeListener) {
-        ProxyHolder<T> proxyHolder = createProxyHolder(wrapped);
-        proxyHolder.getOnPreInvokeListeners().add(onPreInvokeListener);
-        proxyHolder.getOnPostInvokeListeners().add(onPostInvokeListener);
-        return proxyHolder.getProxy();
+        return createProxyHolder(() -> wrapped, wrapped.getClass(), new LinkedList<>(), new LinkedList<>(), new LinkedList<>());
     }
 
     /*
@@ -97,45 +161,57 @@ public class ProxyUtil {
 
         private final Supplier<? extends T> wrapped;
 
-        private Lazy<List<OnPreInvokeListener<? super T>>> onPreInvokeListeners = new AtomicLazy<>(() -> new LinkedList<>());
+        private List<OnPreInvokeListener<? super T>> onPreInvokeListeners;
 
-        private Lazy<List<OnPostInvokeListener<? super T>>> onPostInvokeListeners = new AtomicLazy<>(() -> new LinkedList<>());
+        private List<OnPostInvokeListener<? super T>> onPostInvokeListeners;
 
-        private Lazy<List<ErrorHandler<? super T>>> errorHandlers = new AtomicLazy<>(() -> new LinkedList<>());
+        private List<ErrorHandler<? super T>> errorHandlers;
+
+        public Handler(Supplier<? extends T> wrapped,
+                       List<OnPreInvokeListener<? super T>> onPreInvokeListeners,
+                       List<OnPostInvokeListener<? super T>> onPostInvokeListeners,
+                       List<ErrorHandler<? super T>> errorHandlers) {
+            this.wrapped = wrapped;
+            this.onPreInvokeListeners = onPreInvokeListeners;
+            this.onPostInvokeListeners = onPostInvokeListeners;
+            this.errorHandlers = errorHandlers;
+        }
 
         public Handler(Supplier<? extends T> wrapped) {
-            this.wrapped = wrapped;
+            this(wrapped, new LinkedList<>(), new LinkedList<>(), new LinkedList<>());
         }
 
         @Override
         public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
             // メソッド前インターセプタ
-            if (onPreInvokeListeners.isEvaluated()) {
-                onPreInvokeListeners.get().forEach(listener -> listener.onPreInvoke(wrapped.get(), method, args));
-            }
+            onPreInvokeListeners.forEach(listener -> listener.onPreInvoke(wrapped.get(), method, args));
 
             Object result = ErrorHandler.Result.UNDEFINED;
-            boolean isErrorOccurred = false;
+            Throwable targetException = null;
             try {
                 // メソッドの実行
                 result = method.invoke(wrapped.get(), args);
             } catch (InvocationTargetException e) {
                 // 例外が発生したのでエラーハンドラで処理する。
-                isErrorOccurred = true;
-                if (errorHandlers.isEvaluated()) {
-                    for (ErrorHandler<? super T> errorHandler : errorHandlers.get()) {
-                        ErrorHandler.Result tempResult = errorHandler.onHandleError(wrapped.get(), method, args, e.getTargetException(), result);
-                        result = tempResult.getValue();
-                        if (tempResult.stopFlooding()) break;
-                    }
+                targetException = e.getTargetException();
+
+                //エラーハンドラが１つもなかったら例外をそのままスローする
+                if (errorHandlers.isEmpty()) {
+                    throw targetException;
                 }
+
+                for (ErrorHandler<? super T> errorHandler : errorHandlers) {
+                    ErrorHandler.Result tempResult = errorHandler.onHandleError(wrapped.get(), method, args, targetException, result);
+                    result = tempResult.getValue();
+                    if (tempResult.stopFlooding()) break;
+                }
+
             } finally {
-                if (onPostInvokeListeners.isEvaluated()) {
-                    // ラムダ用に実質的finalな変数に代入する
-                    Object result2 = result;
-                    boolean isErrorOccurred2 = isErrorOccurred;
-                    onPostInvokeListeners.get().forEach(listener -> listener.onPostInvoke(wrapped.get(), method, args, result2, isErrorOccurred2));
-                }
+                // ラムダ用に実質的finalな変数に代入する
+                Object result2 = result;
+                Throwable targetException2 = targetException;
+                onPostInvokeListeners.forEach(listener -> listener.onPostInvoke(wrapped.get(), method, args, result2, targetException2));
+
             }
 
             // 一度も適切な代替戻り値が指定されていない場合、デフォルト値で返す。
@@ -147,15 +223,15 @@ public class ProxyUtil {
         }
 
         public List<OnPostInvokeListener<? super T>> getOnPostInvokeListeners() {
-            return onPostInvokeListeners.get();
+            return onPostInvokeListeners;
         }
 
         public List<OnPreInvokeListener<? super T>> getOnPreInvokeListeners() {
-            return onPreInvokeListeners.get();
+            return onPreInvokeListeners;
         }
 
         public List<ErrorHandler<? super T>> getErrorHandlers() {
-            return errorHandlers.get();
+            return errorHandlers;
         }
     }
 
